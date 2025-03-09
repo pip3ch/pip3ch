16b77107f37496e1dba4436096b8853d7c759232
Poranut Chollavorn <pipech@gmail.com>
2025-03-09 09:19:10 +0000

    chore: init nx

---
+++ apps/pip3ch/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'pip3ch',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/pip3ch',
+};
