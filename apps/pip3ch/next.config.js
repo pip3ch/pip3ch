02ce0bcc841cde1ca7f2c349bad670ebe170b5c8
Poranut Chollavorn <pipech@gmail.com>
2025-07-07 07:00:45 +0000

    feat: add github action for page deployment

--- apps/pip3ch/next.config.js
+++ apps/pip3ch/next.config.js
@@ -7,6 +7,21 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  /**
+ * Enable static exports.
+ *
+ * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+ */
+  output: "export",
+  /**
+ * Disable server-based image optimization. Next.js does not support
+ * dynamic features with static exports.
+ *
+ * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+ */
+  images: {
+    unoptimized: true,
+  },
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
